b245a6aee4471f5a942f79773fa28725336110b6
soumyagowda15 <soumya.gowda@neutrinos.co>
2023-02-14 18:09:48 +0530

    jenkins file

--- Interface/IEngineResults.ts
+++ Interface/IEngineResults.ts
@@ -1,13 +1,14 @@
 import { FormField } from "./IFormField";
 import { Word } from "./IWord";
 import { Classification } from "./IClassification";
+import { Table } from "./ITable";
 export interface EngineResult {
     _id: string;
     ocr: string;
     file_id: string;
     job_id: string;
     form_fields: FormField[];
-    tables: any[];
+    tables?: Table[];
     words: Word[];
     classification: Classification[];
     _n_redacted_text_?: string[];
