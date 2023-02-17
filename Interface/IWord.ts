12f1b8190ec8734cf5cf694e0ef8ebec639362d7
soumyagowda15 <soumya.gowda@neutrinos.co>
2023-02-17 11:17:16 +0530

    Reverted changes

--- Interface/IWord.ts
+++
@@ -1,7 +0,0 @@
-import { Coordinate } from "./ICoordinate";
-export interface Word {
-    pageNumber: number;
-    content: string;
-    coordinates: Coordinate[];
-    confidence: number;
-}
--- readme.txt
+++
@@ -1,2 +0,0 @@
-To run the script use npm run (any script name in the package.json)
-Example npm run E2E
